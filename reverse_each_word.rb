361f81085b54d073051f2f1bbcd389b79eaa8418
Marcos Rodriguez <m.rodrig.felix@gmail.com>
2020-03-10 03:17:24 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -11,15 +11,15 @@
 # turn back into a string
 
 
-# def reverse_each_word(sentence)
-#   new_array = sentence.split(" ")
+def reverse_each_word(sentence)
+  new_array = sentence.split(" ")
   
-#   new_sentence = new_array.collect do |word|         
-#     word.reverse         
-#   end
+  new_sentence = new_array.collect do |word|         
+    word.reverse         
+  end
   
-#   new_sentence.join(" ")
-# end
+  new_sentence.join(" ")
+end
 
 
 
