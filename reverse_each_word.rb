c3081f85a3c8e9603a84d0d9f230ec6c5584aa67
Marcos Rodriguez <m.rodrig.felix@gmail.com>
2020-03-10 02:42:45 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -14,7 +14,7 @@ def reverse_each_word(sentence)
   new_array = sentence.split(" ")
   
   new_array.collect do |word|         
-    word.reverse         
+    puts word.reverse         
   end
   
   new_array.join(" ")
